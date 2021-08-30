30c99a649290645603e4fc3044d786db378df442
Stuart Clark <stuart@webuildbots.ai>
2021-08-30 13:49:47 +0100

    controller: add support for error types

--- src/express/controller.ts
+++ src/express/controller.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
-import { AbstractEndpointDef } from '@src/endpoint';
+import { AbstractEndpointDef, StandardEndpointDef } from '@src/endpoint';
 
 export type TRequest<T extends AbstractEndpointDef> = Request<
   T['requestOptions']['params'],
@@ -8,10 +8,14 @@ export type TRequest<T extends AbstractEndpointDef> = Request<
   T['requestOptions']['query']
 >;
 
-export type TResponse<T extends AbstractEndpointDef> = Response<T['responseBody']>;
+export type TResponse<T extends AbstractEndpointDef> = Response<T['responseBody'] | T['errorType']>;
 
 export type Controller<T extends AbstractEndpointDef> = (
   req: TRequest<T>,
   res: TResponse<T>,
   next?: NextFunction
 ) => void;
+
+export const sendError = <T extends StandardEndpointDef>(res: TResponse<T>, errorType: T['errorType']): void => {
+  res.status(errorType.status).send(errorType);
+};
