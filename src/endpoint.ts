9770d1a2b92c091b0593e61e558da225e52fc65f
Stuart Clark <stuart@webuildbots.ai>
2021-08-30 13:47:33 +0100

    endpoint: ErrorType

--- src/endpoint.ts
+++ src/endpoint.ts
@@ -5,9 +5,17 @@ export interface ReqOptions {
   headers?: Record<string, unknown>;
 }
 
-export interface EndpointDef<ReqOpt extends ReqOptions, RespT> {
-  requestOptions: ReqOpt;
-  responseBody: RespT;
+export interface ErrorType<S extends number = 500> {
+  status: S;
+  msg: string;
 }
 
-export type AbstractEndpointDef = EndpointDef<any, any>;
+export interface EndpointDef<ReqOpt extends ReqOptions, RespT, E = ErrorType> {
+  requestOptions: ReqOpt;
+  responseBody: RespT;
+  errorType: E;
+}
+
+export type StandardEndpointDef = EndpointDef<any, any, ErrorType<any>>;
+
+export type AbstractEndpointDef = EndpointDef<any, any, any>;
