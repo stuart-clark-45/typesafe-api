6427c3c8a1ed907895764a65448d51edaa51433d
Stuart Clark <stuart@webuildbots.ai>
2021-09-01 18:44:10 +0100

    endpoint: add default endpoint

--- src/endpoint.ts
+++ src/endpoint.ts
@@ -1,3 +1,4 @@
+import { Deepmerge } from './type/deepmerge';
 
 /* eslint-disable @typescript-eslint/ban-types */
 // We need to use the {} type here or merging default and endpoint request options doesn't work
@@ -17,12 +18,13 @@ export interface ErrorType<S extends number = 500> {
   msg: string;
 }
 
-export interface EndpointDef<ReqOpt extends ReqOptions, RespT, E = ErrorType> {
-  requestOptions: ReqOpt;
+export interface EndpointDef<DefaultReqOpt extends ReqOptions, ReqOpt extends ReqOptions, RespT, E = ErrorType> {
+  requestOptions: Deepmerge<DefaultReqOpt, ReqOpt>;
+  clientReqOptions: ReqOpt;
   responseBody: RespT;
   errorType: E;
 }
 
-export type StandardEndpointDef = EndpointDef<any, any, ErrorType<any>>;
+export type StandardEndpointDef = EndpointDef<any, any, any, ErrorType<any>>;
 
-export type AbstractEndpointDef = EndpointDef<any, any, any>;
+export type AbstractEndpointDef = EndpointDef<any, any, any, any>;
