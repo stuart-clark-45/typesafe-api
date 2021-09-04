4314ba7e1b0f3177b902c4f33b7b0595dffbf1e6
Stuart Clark <stuart@webuildbots.ai>
2021-09-04 02:00:54 +0100

    compiler.test: created

---
+++ test/compiler-test/compiler.test.ts
@@ -0,0 +1,22 @@
+import { getCompilerErrors, getTestFiles } from './util';
+import { parseTS } from './parse-ts';
+
+type AllFileErrors = {
+  [key: string]: string[];
+};
+
+it('test', async () => {
+  const expected: AllFileErrors = {};
+  const actual: AllFileErrors = {};
+
+  for (const file of await getTestFiles()) {
+    const errors = await getCompilerErrors(file);
+    // Add "// " to make them the same as the parsed errors
+    actual[file] = errors.map((s) => `// ${s}`);
+
+    const { expectedErrors } = await parseTS(file);
+    expected[file] = expectedErrors.slice(1);
+  }
+
+  expect(actual).toEqual(expected);
+}, 30000);
